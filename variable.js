0c0b174c64779e41c2c0d9a11ff63fc20151d064
rupayan <rupbera922@gmail.com>
2026-05-24 00:00:04 +0530

    const variable understanding

--- variable.js
+++ variable.js
@@ -1,3 +1,42 @@
 // Javascript is mainly build for client side
 
 console.log("Hello My name is Rupayan Bera");
+
+// variable is a named memory location 
+
+// var 
+var myName = "Rupayan"; // Global Scoped 
+
+{
+    var secondName = "Love Babbar";
+}
+console.log(secondName); 
+
+function testvar (){
+    var thirdName = "Lakshay";
+    
+}
+testvar()
+// console.log(thirdName);
+
+
+// Let 
+{
+let nusaiba = 23;
+}
+
+// console.log(nusaiba);
+
+function smit (){
+    let nusaiba = 23;
+    console.log(nusaiba);
+}
+smit();
+
+
+
+// const 
+const helloji = "Rupayan"; // const's values canot be edited 
+console.log(helloji); 
+helloji = "Nusaiba";
+console.log(helloji); // const variable can not be redeclered 
